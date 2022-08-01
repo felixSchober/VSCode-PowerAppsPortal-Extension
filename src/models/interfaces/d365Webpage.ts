e80b9450c9a68edecd22087b7ac18e032d896d67
Felix Schober <github@felix-schober.com>
2022-08-01 19:20:45 +0100

    fix encoding issues

--- src/models/interfaces/d365Webpage.ts
+++ src/models/interfaces/d365Webpage.ts
@@ -1,15 +1,14 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 export interface ID365Webpage {
-	adx_webpageid: string;
-	adx_name: string;
-	adx_partialurl: string;
-	_adx_parentpageid_value: string | undefined;
-	_adx_websiteid_value: string | undefined;
-	_adx_publishingstateid_value: string | undefined;
-	adx_hiddenfromsitemap: boolean | undefined;
-	_adx_pagetemplateid_value: string | undefined;
-	
-
+    adx_webpageid: string;
+    adx_name: string;
+    adx_isroot: boolean;
+    adx_partialurl: string;
+    _adx_parentpageid_value: string | undefined;
+    _adx_websiteid_value: string | undefined;
+    _adx_publishingstateid_value: string | undefined;
+    adx_hiddenfromsitemap: boolean | undefined;
+    _adx_pagetemplateid_value: string | undefined;
 }
 
-export const WEBPAGE_SELECT = ['adx_webpageid', 'adx_name', 'adx_partialurl', '_adx_parentpageid_value'];
+export const WEBPAGE_SELECT = ["adx_webpageid", "adx_name", "adx_partialurl", "_adx_parentpageid_value", "adx_isroot"];
